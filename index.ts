b124052ae3a8327d7412ad7999c57d1be55733cc
soudblox <nick8x@gmail.com>
2025-01-29 20:54:54 +0700

    feat(backend): add logging, env, error handling

--- index.ts
+++ index.ts
@@ -1,15 +1,15 @@
+import "dotenv/config";
 import express, { Express } from "express";
 import path from "path";
+import morgan from "morgan";
 import * as cheerio from "cheerio";
 
 const app: Express = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
+app.use(morgan("dev"));
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "../public")));
-app.get("/hi", (_, res) => {
-	res.send("Express + TypeScript Server");
-});
 
 app.post("/format", (req, res) => {
 	const { text }: FormatInput = req.body;
@@ -35,27 +35,37 @@ app.post("/scrape", async (req, res) => {
 app.post("/searchFormatted", async (req, res) => {
 	const { query } = req.body;
 	const results = await searchSongs(query);
+	if (!results) {
+		res.status(404).send();
+		return;
+	}
 	const lyrics = await scrapeLyrics(results);
+	if (!lyrics) {
+		res.status(404).send();
+		return;
+	}
 	const formatted = lyricsFormatter(lyrics);
 	res.send(formatted);
 })
 
 app.listen(port, () => {
-	console.log(`[server]: Server is running at http://localhost:${port}`);
+	console.log(`[server]: Server is running at port ${port}`);
 });
 
-async function searchSongs(input: string): Promise<string> {
-	const res = await fetch(`https://lagumisakatolik.com/?s=${input}`);
+async function searchSongs(input: string): Promise<string | undefined> {
+	// temporary until i fix my server
+	const res = await fetch(`https://ppsntr.nick8x.workers.dev/?__url=https://lagumisakatolik.com/?s=${input}&ct_post_type=post%3Apage`);
 	const body = await res.text();
 
 	const $ = cheerio.load(body);
 	const attr = $(".entry-title a")
-	const link = attr.attr("href") || "undefined";
+	const link = attr.attr("href") || undefined;
 	return link;
 };
 
-async function scrapeLyrics(link: string): Promise<string> {
-	const res = await fetch(link);
+async function scrapeLyrics(link: string): Promise<string | undefined> {
+	if (!link) return;
+	const res = await fetch(`https://ppsntr.nick8x.workers.dev/?__url=${link}`);
 	const body = await res.text();
 
 	const $ = cheerio.load(body);
@@ -81,5 +91,5 @@ function lyricsFormatter(input: string): string {
 		}
 		final.push(i);
 	}
-	return final.join("\n");
+	return final.join("\n").trim();
 }
